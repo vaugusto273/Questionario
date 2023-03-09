e516a76ecc45786c6ce9c7a135f87483904605b7
vaugusto273 <samlara273@gmail.com>
2023-03-08 21:18:18 -0300

    Questão 1 :white_check_mark:

--- Questionario08032023.js
+++ Questionario08032023.js
@@ -1,1 +1,29 @@
-//initial commit
+/*1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+
+K = K + 1;
+
+SOMA = SOMA + K;
+
+}
+
+imprimir(SOMA);
+
+
+
+Ao final do processamento, qual será o valor da variável SOMA?
+*/
+
+let INDICE = 13; SOMA = 0; K = 0;
+
+while(K < INDICE){
+    K++; //Mesma coisa que K = K + 1
+    SOMA = SOMA + K;
+    //console.log(K, SOMA);
+}
+console.log(SOMA);
